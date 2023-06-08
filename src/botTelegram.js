0853abaf9f442f70f8349ed8604193e8280141d7
Cristian Quesada Cossio <cristian.quesada@utp.edu.co>
2023-06-08 15:02:28 -0500

    fix: Log in para la plataforma

--- src/botTelegram.js
+++ src/botTelegram.js
@@ -1,8 +1,9 @@
 import { Telegraf } from "telegraf";
 import { message } from 'telegraf/filters';
-import * as dotenv from 'dotenv' 
+import * as dotenv from 'dotenv'
 import { readHTML } from "./util/extractValues.js"
-import { scraping } from "./util/scraper.js"
+import { goToPage, historicGradesScraping, logInScraping } from "./util/scraper.js"
+import { HTTPResponse } from "puppeteer";
 dotenv.config()
 /*
 const {
@@ -10,43 +11,58 @@ const {
     ENVIRONMENT
 } = process.env
 */
-const URL_BOT = process.env;
-const bot = new Telegraf(URL_BOT);
+//const URL_BOT = process.env;
+const GRADES_PAGE_URL = "https://app4.utp.edu.co/reportes/ryc/ReporteDetalladoNotasxEstudiante.php";
+const HISTORIC_PAGE_URL = "https://app4.utp.edu.co/MatAcad/verificacion/historial-web/programas.php";
+
+const bot = new Telegraf("6151207917:AAFQ9aHQJcPb5qmFRe__GJznRC-jAe0gDoE");
 
 bot.use((ctx, next) => {
   ctx.state.users = 75;
-  next(ctx); 
+  next(ctx);
 })
 
 bot.start((ctx) => {
   ctx.reply('Welcome');
-  ctx.reply('Para usar el bot usa el siguiente comando\n /notas cedula contraseña\nLos datos del portal estudiantil')
-  
+  ctx.reply(`Para usar el bot alguno de los siguientes comando\n
+    /notas cedula contraseña\n
+    /promedio cedula contraeña\n
+    Los datos del portal estudiantil`)
+
 })
 
 bot.help(ctx => ctx.reply('help command'))
 
 bot.settings(ctx => ctx.reply('settings command'))
 
+// Command for current semester grades.
 bot.command([/notas.*/], async (ctx) => {
-    let inforeplymessage
-    ctx.reply('Vamos a procesar su peticion, esto puede tardar algunos minutos.').then(data => inforeplymessage = data)
-    console.log("Respondiendo a",ctx.from.first_name, ctx.from.username)
-    const input = ctx.update.message.text.split(" ")
-    if(input.length != 3){
-      ctx.reply('No ingreso bien los datos /nDebe ser /notas [Cedula] [contraseña]')
-    }else {
-      const username = input[1]
-      const password = input[2]
-      const page = await scraping(username, password)
+  let inforeplymessage
+  ctx.reply('Vamos a procesar su peticion, esto puede tardar algunos minutos.').then(data => inforeplymessage = data)
+  console.log("Respondiendo a", ctx.from.first_name, ctx.from.username)
+  const input = ctx.update.message.text.split(" ")
+  if (input.length != 3) {
+    ctx.reply('No ingreso bien los datos /nDebe ser /notas [Cedula] [contraseña]')
+  } else {
+    const username = input[1];
+    const password = input[2];
+
+    try {
+      const page = await logInScraping(username, password);
+      await page.goto(GRADES_PAGE_URL);
       const values = await readHTML(page);
-      page.close();
-      for(const ms of values){
+      console.log(values);
+      for (const ms of values) {
         ctx.reply(ms)
       }
+      page.close();
+    } catch (error) {
+      console.log("Error: " + error);
     }
-    ctx.deleteMessage(ctx.update.message.message_id)
-    // ctx.deleteMessage(inforeplymessage.message_id)
+  }
+  //ctx.deleteMessage(ctx.update.message.message_id)
 })
+
+
 
 bot.launch()
--- src/util/scraper.js
+++ src/util/scraper.js
@@ -3,14 +3,14 @@ import * as randomUseragent from "random-useragent";
 import { message } from "telegraf/filters";
 // import keytest from '../keytest.json' assert {type: 'json'};
 
-const scraping = async (user, password) => {
+const logInScraping = async (user, password) => {
 
     const header = randomUseragent.getRandom((ua) => {
         return ua.browserName == 'Firefox'; 
     });
 
     const browser = await puppeteer.launch({
-        headless: true, 
+        headless: false, 
         ignoreHTTPSErrors: true,
     });
 
@@ -30,33 +30,24 @@ const scraping = async (user, password) => {
 
     await page.click("#enviar");
 
-    await page.waitForNavigation()
+    await page.waitForNavigation();
+    
+    return page; // returns portal home page (After log in)
 
-    await page.goto("https://app4.utp.edu.co/reportes/ryc/ReporteDetalladoNotasxEstudiante.php", {timeout: 0})
+    //await page.goto("https://app4.utp.edu.co/reportes/ryc/ReporteDetalladoNotasxEstudiante.php", {timeout: 0})
 
     // console.log(await page.$("//html/body/table/tbody/tr[4]/td/table/tbody/tr/td/table/tbody/tr[3]/td/table"))
-    return page;
+    //return page;
 }
 
+const goToPage = async (homePage, pageUrl) => {
+    return await homePage.goto(pageUrl);
+}
 
+const historicGradesScraping = async (programsPage) => {
+    const comboBoxPrograms = await programsPage.waitForSelector('#cmbprogramas');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 // scraping('1004995317', 'MatiaS!181120.');
-export { scraping }
+export { logInScraping, goToPage, historicGradesScraping};
